192d555441063fddd29c85869d475fdce127f2a0
hrickbel <henrique.belemdosantos@gmail.com>
2024-07-04 10:15:22 -0300

    refactured_homepage

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,33 +1,5 @@
-function openSidebar(){
-    let sidebar = $(".sidebar");
-    let shade = $(".shade");
+function expand(n){
+    let expansable = document.getElementById(String(n));
 
-    if(sidebar.css("display") === "none"){
-        shade.show();
-        sidebar.css("display","flex");
-    }else{
-        shade.hide();
-        sidebar.css("display","none");
-    }
-}
-function closeSidebar(){
-    let sidebar = $(".sidebar");
-    let shade = $(".shade");
-
-    if(sidebar.css("display") != "none"){
-        shade.hide();
-        sidebar.css("display","none");
-    }
-}
-
-function expand(id){
-    let dom = $(id);
-
-    if(dom.attr("value") === "hide"){
-        dom.attr({"value":"show"});
-        dom.show();
-    }else{
-        dom.attr({"value":"hide"})
-        dom.hide();
-    }
+    expansable.hidden = !expansable.hidden
 }
